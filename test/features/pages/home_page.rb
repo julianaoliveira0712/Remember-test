ac4ed2ba86fc3d69125eb7a7290f8861a5a57c1d
Juliana Souza de Oliveira <JULIANASOU@cateno.com.br>
2019-11-27 13:57:36 -0300

    homeokay/memoryline/moment e comment

--- test/features/pages/home_page.rb
+++ test/features/pages/home_page.rb
@@ -1,36 +1,48 @@
 class Home_page < SitePrism::Page
     set_url 'userhome'
 
-    element :navbar, '.MuiPaper-root MuiPaper-elevation4 MuiAppBar-root MuiAppBar-positionFixed jss295 MuiAppBar-colorPrimary mui-fixed'
-    element :perfil, '.MuiButtonBase-root MuiButton-root MuiButton-text jss300 [aria-controls="simple-menu"]'
-    element :dropdown, '.MuiList-root MuiList-padding [role="menu"]'
-    element :sair, '.MuiList-root MuiList-padding [text="Sair"]'
-    element :button_sair, '.MuiPaper-root MuiPaper-elevation1 jss685 MuiPaper-rounded'
-    element :button_add_memoryline, '.MuiSvgIcon-root [focusable="false"]'
-    element :ver_memoryline_private, '.MuiSvgIcon-root jss1434 jss1435 [role="presentation"]'
-    element :memoryline, 'a[href="/memoryline/?ref=5dc17b59527bb98d2f31a66d&title=Memory Line Vazia"]'
-
+    element :remember, 'p[to="/userhome"]'
+    element :perfil, 'button[aria-controls="simple-menu"]'
+    element :add_memoryline, 'button[aria-label="add"]'
+    element :memoryline, 'a[href="/memoryline/?ref=5ddb3c1c4cfb20ff61b7b057&title=HxH"]'
+    element :sair, '#sair'
+    element :popupCriarMemoryline, '#form-dialog-title'
+    element :comment, '#comment-content'
     def verificar_page
-        find(navbar)
+        assert_text('remember')
+    end
+
+    def dropdown(p)
+        click_button p 
     end
 
     def add_memoryline
-        button_add_memoryline.click
-    end
-
-    def ver_memoryline_private
-        ver_memoryline_private.click
+        add_memoryline.click
     end
 
     def sair
         perfil.click
-    end
-    
-    def sair
         sair.click
     end
-
-    def memoryline_create_sucess
-        find(memoryline)
+    
+    def entrarMemoryline
+        memoryline.click
     end
+
+    def popupCriarMemoryline
+        assert_text("Nome da MemoryLine")
+        click_button 'CRIAR'
+    end
+
+    def verMoment(id)
+        find(id).click
+    end
+
+    def fazerComment
+        fill_in comment, with: 'garantindo o 10'
+        element.send_keys :enter
+    end
+
+
+
 end
